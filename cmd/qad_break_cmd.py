fa72befaf46b4f49b048e3114866a99af6c68fb1
brad-blackruby <brad@blackruby.dev>
2025-05-10 23:33:12 -0500

    Update qad_break_cmd.py
    translated comments

--- cmd/qad_break_cmd.py
+++ cmd/qad_break_cmd.py
@@ -3,13 +3,13 @@
 /***************************************************************************
  QAD Quantum Aided Design plugin
 
- comando SPEZZA per tagliare un oggetto 
+ BREAK command to cut an object
  
                               -------------------
-        begin                : 2014-01-09
+        begin                : 2025-05-10
         copyright            : iiiii
-        email                : hhhhh
-        developers           : bbbbb aaaaa ggggg
+        email                : brad@blackruby.dev
+        developers           : Brad, ClaudeAI
  ***************************************************************************/
 
 /***************************************************************************
@@ -38,11 +38,11 @@ from ..qad_break_fun import breakQadGeometry
 from ..qad_multi_geom import fromQadGeomToQgsGeom, setQadGeomAt
 
 
-# Classe che gestisce il comando BREAK
+# Class that manages the BREAK command
 class QadBREAKCommandClass(QadCommandClass):
 
    def instantiateNewCmd(self):
-      """ istanzia un nuovo comando dello stesso tipo """
+      """ instantiates a new command of the same type """
       return QadBREAKCommandClass(self.plugIn)
 
    def getName(self):
@@ -58,7 +58,7 @@ class QadBREAKCommandClass(QadCommandClass):
       return QIcon(":/plugins/qad/icons/break.svg")
    
    def getNote(self):
-      # impostare le note esplicative del comando      
+      # set the explanatory notes of the command      
       return QadMsg.translate("Command_BREAK", "Breaks an object.")
    
    def __init__(self, plugIn):
@@ -74,14 +74,14 @@ class QadBREAKCommandClass(QadCommandClass):
          del self.entSelClass
 
    def getPointMapTool(self, drawMode = QadGetPointDrawModeEnum.NONE):
-      if self.step == 1: # quando si é in fase di selezione entità
+      if self.step == 1: # when in entity selection phase
          return self.entSelClass.getPointMapTool(drawMode)
       else:
          return QadCommandClass.getPointMapTool(self, drawMode)
 
 
    def getCurrentContextualMenu(self):
-      if self.step == 1: # quando si é in fase di selezione entità
+      if self.step == 1: # when in entity selection phase
          return self.entSelClass.getCurrentContextualMenu()
       else:
          return self.contextualMenu
@@ -93,7 +93,7 @@ class QadBREAKCommandClass(QadCommandClass):
       self.step = 1         
       self.entSelClass = QadEntSelClass(self.plugIn)
       self.entSelClass.msg = QadMsg.translate("Command_BREAK", "Select the object to break: ")
-      # scarto la selezione di punti e poligoni
+      # exclude selection of points and polygons
       self.entSelClass.checkPointLayer = False
       self.entSelClass.checkLineLayer = True
       self.entSelClass.checkPolygonLayer = False
@@ -129,7 +129,7 @@ class QadBREAKCommandClass(QadCommandClass):
                self.plugIn.destroyEditCommand()
                return
             brokenFeature1 = QgsFeature(f)
-            # trasformo la geometria nel crs del layer
+            # transform the geometry into the layer CRS
             brokenFeature1.setGeometry(fromQadGeomToQgsGeom(updGeom, layer))
             # plugIn, layer, feature, refresh, check_validity
             if qad_layer.updateFeatureToLayer(self.plugIn, layer, brokenFeature1, False, False) == False:
@@ -137,14 +137,14 @@ class QadBREAKCommandClass(QadCommandClass):
                return
          if line2 is not None:
             brokenFeature2 = QgsFeature(f)      
-            # trasformo la geometria nel crs del layer
+            # transform the geometry into the layer CRS
             brokenFeature2.setGeometry(fromQadGeomToQgsGeom(line2, layer))
             # plugIn, layer, feature, coordTransform, refresh, check_validity
             if qad_layer.addFeatureToLayer(self.plugIn, layer, brokenFeature2, None, False, False, False) == False:
                self.plugIn.destroyEditCommand()
                return            
       else:
-         # aggiungo le linee nei layer temporanei di QAD
+         # add the lines to the QAD temporary layers
          if LineTempLayer is None:
             LineTempLayer = qad_layer.createQADTempLayer(self.plugIn, QgsWkbTypes.LineGeometry)
             self.plugIn.addLayerToLastEditCommand("Feature broken", LineTempLayer)
@@ -155,7 +155,7 @@ class QadBREAKCommandClass(QadCommandClass):
          if line2 is not None:
             lineGeoms.append(fromQadGeomToQgsGeom(line2, layer))
 
-         # trasformo la geometria in quella dei layer temporanei
+         # transform the geometry to that of the temporary layers
          # plugIn, pointGeoms, lineGeoms, polygonGeoms, coord, refresh
          if qad_layer.addGeometriesToQADTempLayers(self.plugIn, None, lineGeoms, None, None, False) == False:
             self.plugIn.destroyEditCommand()
@@ -163,14 +163,14 @@ class QadBREAKCommandClass(QadCommandClass):
          
          updGeom = delQadGeomAt(qadGeom, atGeom, atSubGeom)
 
-         if updGeom == False: # da cancellare
+         if updGeom == False: # to be deleted
             # plugIn, layer, feature id, refresh
             if qad_layer.deleteFeatureToLayer(self.plugIn, layer, f.id(), False) == False:
                self.plugIn.destroyEditCommand()
                return
          else:
             brokenFeature1 = QgsFeature(f)
-            # trasformo la geometria nel crs del layer
+            # transform the geometry into the layer CRS
             brokenFeature1.setGeometry(fromQadGeomToQgsGeom(updGeom, layer))
             # plugIn, layer, feature, refresh, check_validity
             if qad_layer.updateFeatureToLayer(self.plugIn, layer, brokenFeature1, False, False) == False:
@@ -183,14 +183,14 @@ class QadBREAKCommandClass(QadCommandClass):
    def run(self, msgMapTool = False, msg = None):
       if self.plugIn.canvas.mapSettings().destinationCrs().isGeographic():
          self.showMsg(QadMsg.translate("QAD", "\nThe coordinate reference system of the project must be a projected coordinate system.\n"))
-         return True # fine comando
+         return True # end command
       
       if self.step == 0:     
          self.waitForEntsel(msgMapTool, msg)
-         return False # continua
+         return False # continue
       
       # =========================================================================
-      # RISPOSTA ALLA SELEZIONE DI UN'ENTITA' (da step = 0)
+      # RESPONSE TO THE SELECTION OF AN ENTITY (from step = 0)
       elif self.step == 1:
          if self.entSelClass.run(msgMapTool, msg) == True:
             if self.entSelClass.entity.isInitialized():
@@ -202,48 +202,48 @@ class QadBREAKCommandClass(QadCommandClass):
                prompt = QadMsg.translate("Command_BREAK", "Specify second break point or [{0}]: ").format(keyWords)
                
                self.step = 2
-               self.getPointMapTool().refreshSnapType() # aggiorno lo snapType che può essere variato dal maptool di selezione entità
+               self.getPointMapTool().refreshSnapType() # update the snapType which may have been modified by the entity selection maptool
                                    
                englishKeyWords = "First point"
                keyWords += "_" + englishKeyWords
-               # si appresta ad attendere un punto o enter o una parola chiave         
-               # msg, inputType, default, keyWords, nessun controllo
+               # prepares to wait for a point or enter or a keyword      
+               # msg, inputType, default, keyWords, no check
                self.waitFor(prompt, \
                             QadInputTypeEnum.POINT2D | QadInputTypeEnum.KEYWORDS, \
                             None, \
                             keyWords, QadInputModeEnum.NONE)      
                return False
             else:
-               if self.entSelClass.canceledByUsr == True: # fine comando
+               if self.entSelClass.canceledByUsr == True: # end command
                   return True
                self.showMsg(QadMsg.translate("QAD", "No geometries in this position."))
                self.waitForEntsel(msgMapTool, msg)
-         return False # continua
+         return False # continue
 
       # =========================================================================
-      # RISPOSTA ALLA RICHIESTA DEL SECONDO PUNTO DI INTERRUZIONE (da step = 1)
-      elif self.step == 2: # dopo aver atteso un punto o una parola chiave si riavvia il comando
-         if msgMapTool == True: # il punto arriva da una selezione grafica
-            # la condizione seguente si verifica se durante la selezione di un punto
-            # é stato attivato un altro plugin che ha disattivato Qad
-            # quindi stato riattivato il comando che torna qui senza che il maptool
-            # abbia selezionato un punto            
-            if self.getPointMapTool().point is None: # il maptool é stato attivato senza un punto
-               if self.getPointMapTool().rightButton == True: # se usato il tasto destro del mouse
-                  pass # opzione di default "secondo punto"
+      # RESPONSE TO THE REQUEST FOR THE SECOND BREAK POINT (from step = 1)
+      elif self.step == 2: # after waiting for a point or a keyword, restart the command
+         if msgMapTool == True: # the point comes from a graphic selection
+            # the following condition occurs if during the selection of a point
+            # another plugin that has disabled Qad has been activated
+            # then the command has been reactivated which returns here without the maptool
+            # having selected a point            
+            if self.getPointMapTool().point is None: # the maptool has been activated without a point
+               if self.getPointMapTool().rightButton == True: # if used the right mouse button
+                  pass # default option "second point"
                else:
-                  self.setMapTool(self.getPointMapTool()) # riattivo il maptool
+                  self.setMapTool(self.getPointMapTool()) # reactivate the maptool
                   return False
 
             value = self.getPointMapTool().point
-         else: # il punto arriva come parametro della funzione
+         else: # the point comes as a parameter of the function
             value = msg
 
          if value is None or type(value) == unicode:
-            # si appresta ad attendere un punto
+            # prepares to wait for a point
             self.waitForPoint(QadMsg.translate("Command_BREAK", "Specify first break point: "))            
             self.step = 3
-         elif type(value) == QgsPointXY: # se é stato inserito il secondo punto
+         elif type(value) == QgsPointXY: # if the second point has been entered
             self.secondPt = value
             self.plugIn.setLastPoint(self.secondPt)
             self.breakFeatures()            
@@ -252,50 +252,50 @@ class QadBREAKCommandClass(QadCommandClass):
          return False 
 
       # =========================================================================
-      # RISPOSTA ALLA RICHIESTA DEL PRIMO PUNTO DI INTERRUZIONE (da step = 2)
-      elif self.step == 3: # dopo aver atteso un punto si riavvia il comando
-         if msgMapTool == True: # il punto arriva da una selezione grafica
-            # la condizione seguente si verifica se durante la selezione di un punto
-            # é stato attivato un altro plugin che ha disattivato Qad
-            # quindi stato riattivato il comando che torna qui senza che il maptool
-            # abbia selezionato un punto            
-            if self.getPointMapTool().point is None: # il maptool é stato attivato senza un punto
-               if self.getPointMapTool().rightButton == True: # se usato il tasto destro del mouse
-                  return True # fine comando
+      # RESPONSE TO THE REQUEST FOR THE FIRST BREAK POINT (from step = 2)
+      elif self.step == 3: # after waiting for a point, restart the command
+         if msgMapTool == True: # the point comes from a graphic selection
+            # the following condition occurs if during the selection of a point
+            # another plugin that has disabled Qad has been activated
+            # then the command has been reactivated which returns here without the maptool
+            # having selected a point            
+            if self.getPointMapTool().point is None: # the maptool has been activated without a point
+               if self.getPointMapTool().rightButton == True: # if used the right mouse button
+                  return True # end command
                else:
-                  self.setMapTool(self.getPointMapTool()) # riattivo il maptool
+                  self.setMapTool(self.getPointMapTool()) # reactivate the maptool
                   return False
 
             value = self.getPointMapTool().point
-         else: # il punto arriva come parametro della funzione
+         else: # the point comes as a parameter of the function
             value = msg
 
          self.firstPt =  value
          self.plugIn.setLastPoint(self.firstPt)
 
-         # si appresta ad attendere un punto
+         # prepares to wait for a point
          self.waitForPoint(QadMsg.translate("Command_BREAK", "Specify second break point: "))
          self.step = 4
          
          return False
 
       # =========================================================================
-      # RISPOSTA ALLA RICHIESTA DEL SECONDO PUNTO DI INTERRUZIONE (da step = 3)
-      elif self.step == 4: # dopo aver atteso un punto si riavvia il comando
-         if msgMapTool == True: # il punto arriva da una selezione grafica
-            # la condizione seguente si verifica se durante la selezione di un punto
-            # é stato attivato un altro plugin che ha disattivato Qad
-            # quindi stato riattivato il comando che torna qui senza che il maptool
-            # abbia selezionato un punto            
-            if self.getPointMapTool().point is None: # il maptool é stato attivato senza un punto
-               if self.getPointMapTool().rightButton == True: # se usato il tasto destro del mouse
-                  return True # fine comando
+      # RESPONSE TO THE REQUEST FOR THE SECOND BREAK POINT (from step = 3)
+      elif self.step == 4: # after waiting for a point, restart the command
+         if msgMapTool == True: # the point comes from a graphic selection
+            # the following condition occurs if during the selection of a point
+            # another plugin that has disabled Qad has been activated
+            # then the command has been reactivated which returns here without the maptool
+            # having selected a point            
+            if self.getPointMapTool().point is None: # the maptool has been activated without a point
+               if self.getPointMapTool().rightButton == True: # if used the right mouse button
+                  return True # end command
                else:
-                  self.setMapTool(self.getPointMapTool()) # riattivo il maptool
+                  self.setMapTool(self.getPointMapTool()) # reactivate the maptool
                   return False
 
             value = self.getPointMapTool().point
-         else: # il punto arriva come parametro della funzione
+         else: # the point comes as a parameter of the function
             value = msg
 
          self.secondPt = value
