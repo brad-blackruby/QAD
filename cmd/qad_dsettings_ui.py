37ba951da0fe48f99ceb98091d7392e12ef7d68a
brad-blackruby <brad@blackruby.dev>
2025-05-15 22:04:37 -0500

    Update qad_dsettings_ui.py
    added comment header - no translation needed

--- cmd/qad_dsettings_ui.py
+++ cmd/qad_dsettings_ui.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ QAD Quantum Aided Design plugin
+
+ DSETTINGS command for drawing settings
+ 
+                              -------------------
+        last update          : 2025-05-15
+        copyright            : iiiii
+        email                : brad@blackruby.dev
+        developers           : Brad, ClaudeAI
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 
 # Form implementation generated from reading ui file '.\cmd\qad_dsettings.ui'
 #
