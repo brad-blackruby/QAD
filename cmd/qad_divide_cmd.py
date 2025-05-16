e7c2ad4b09ee5440370eb31054ddb9fae50cf1ea
brad-blackruby <brad@blackruby.dev>
2025-05-15 21:57:11 -0500

    Update qad_divide_cmd.py
    translated and first update

--- cmd/qad_divide_cmd.py
+++ cmd/qad_divide_cmd.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- QAD Quantum Aided Design plugin ok
+ QAD Quantum Aided Design plugin
 
- comando DIVIDE per creare oggetti puntuali a distanza uguale lungo il perimetro o la lunghezza di un oggetto
+ DIVIDE command to create point objects at equal distances along the perimeter or length of an object
  
                               -------------------
-        begin                : 2016-09-09
+        last update          : 2025-05-15
         copyright            : iiiii
-        email                : hhhhh
-        developers           : bbbbb aaaaa ggggg
+        email                : brad@blackruby.dev
+        developers           : Brad, ClaudeAI
  ***************************************************************************/
 
 /***************************************************************************
@@ -43,16 +43,16 @@ from ..qad_geom_relations import getQadGeomClosestPart
 # QadDIVIDECommandClassStepEnum class.
 # ===============================================================================
 class QadDIVIDECommandClassStepEnum():
-   ASK_FOR_ENT        = 1 # richiede la selezione di un oggetto (0 è l'inizio del comando)
-   ASK_FOR_ALIGNMENT  = 2 # richiede l'allineamento
-   ASK_SEGMENT_NUMBER = 3 # richiede il numero di segmenti
+   ASK_FOR_ENT        = 1 # requests the selection of an object (0 is the start of the command)
+   ASK_FOR_ALIGNMENT  = 2 # requests alignment
+   ASK_SEGMENT_NUMBER = 3 # requests the number of segments
    
 
-# Classe che gestisce il comando DIVIDE
+# Class that manages the DIVIDE command
 class QadDIVIDECommandClass(QadCommandClass):
 
    def instantiateNewCmd(self):
-      """ istanzia un nuovo comando dello stesso tipo """
+      """ instantiates a new command of the same type """
       return QadDIVIDECommandClass(self.plugIn)
    
    def getName(self):
@@ -68,7 +68,7 @@ class QadDIVIDECommandClass(QadCommandClass):
       return QIcon(":/plugins/qad/icons/divide.svg")
 
    def getNote(self):
-      # impostare le note esplicative del comando
+      # set explanatory notes for the command
       return QadMsg.translate("Command_DIVIDE", "Creates evenly spaced punctual objects along the length or perimeter of an object.")
    
    def __init__(self, plugIn):
@@ -93,7 +93,7 @@ class QadDIVIDECommandClass(QadCommandClass):
       self.step = QadDIVIDECommandClassStepEnum.ASK_FOR_ENT
       self.entSelClass = QadEntSelClass(self.plugIn)
       self.entSelClass.msg = QadMsg.translate("Command_DIVIDE", "Select object to divide: ")
-      # scarto la selezione di punti
+      # discard point selection
       self.entSelClass.checkPointLayer = False
       self.entSelClass.checkLineLayer = True
       self.entSelClass.checkPolygonLayer = True
@@ -116,7 +116,7 @@ class QadDIVIDECommandClass(QadCommandClass):
       englishKeyWords = "Yes" + "/" + "No"
       keyWords += "_" + englishKeyWords
 
-      # msg, inputType, default, keyWords, nessun controllo
+      # msg, inputType, default, keyWords, no validation
       self.waitFor(prompt, \
                    QadInputTypeEnum.KEYWORDS, \
                    self.defaultValue, \
@@ -129,9 +129,9 @@ class QadDIVIDECommandClass(QadCommandClass):
    def waitForSegmentNumber(self):
       self.step = QadDIVIDECommandClassStepEnum.ASK_SEGMENT_NUMBER
 
-      # si appresta ad attendere un numero intero
+      # prepares to wait for an integer number
       msg = QadMsg.translate("Command_DIVIDE", "Enter the number of segments: ")
-      # msg, inputType, default, keyWords, valori positivi
+      # msg, inputType, default, keyWords, positive values
       self.waitFor(msg, \
                    QadInputTypeEnum.INT, \
                    None, \
@@ -147,12 +147,12 @@ class QadDIVIDECommandClass(QadCommandClass):
       g = QgsGeometry.fromPointXY(transformedPoint)
       f = QgsVectorLayerUtils.createFeature(layer, g, {}, layer.createExpressionContext())
       
-      # se la scala dipende da un campo 
+      # if scale depends on a field
       scaleFldName = qad_layer.get_symbolScaleFieldName(layer)
       if len(scaleFldName) > 0:
          f.setAttribute(scaleFldName, 1.0)
       
-      # se la rotazione dipende da un campo
+      # if rotation depends on a field
       rotFldName = qad_layer.get_symbolRotationFieldName(layer)
       if len(rotFldName) > 0:
          f.setAttribute(rotFldName, qad_utils.toDegrees(rot))
@@ -171,16 +171,16 @@ class QadDIVIDECommandClass(QadCommandClass):
       layer = self.entSelClass.entity.layer
       
       qadGeom = self.entSelClass.entity.getQadGeom()
-      # la funzione ritorna una lista con 
-      # (<minima distanza>
-      # <punto più vicino>
-      # <indice della geometria più vicina>
-      # <indice della sotto-geometria più vicina>
-      # se geometria chiusa è tipo polyline la lista contiene anche
-      # <indice della parte della sotto-geometria più vicina>
-      # <"a sinistra di" se il punto é alla sinista della parte (< 0 -> sinistra, > 0 -> destra)
+      # the function returns a list with
+      # (<minimum distance>
+      # <closest point>
+      # <index of closest geometry>
+      # <index of the closest sub-geometry>
+      # if closed geometry is polyline type, the list also contains
+      # <index of the part of the closest sub-geometry>
+      # <"left of" if the point is to the left of the part (< 0 -> left, > 0 -> right)
       dummy = getQadGeomClosestPart(qadGeom, self.entSelClass.point)
-      # ritorna la sotto-geometria
+      # returns the sub-geometry
       pathPolyline = getQadGeomAt(qadGeom, dummy[2], dummy[3])
       distance = pathPolyline.length() / self.nSegments
       
@@ -204,61 +204,61 @@ class QadDIVIDECommandClass(QadCommandClass):
    def run(self, msgMapTool = False, msg = None):
       if self.plugIn.canvas.mapSettings().destinationCrs().isGeographic():
          self.showMsg(QadMsg.translate("QAD", "\nThe coordinate reference system of the project must be a projected coordinate system.\n"))
-         return True # fine comando
+         return True # end command
       
       currLayer, errMsg = qad_layer.getCurrLayerEditable(self.plugIn.canvas, QgsWkbTypes.PointGeometry)
       if currLayer is None:
          self.showErr(errMsg)
-         return True # fine comando
+         return True # end command
 
       if qad_layer.isSymbolLayer(currLayer) == False :
          errMsg = QadMsg.translate("QAD", "\nCurrent layer is not a symbol layer.")
          errMsg = errMsg + QadMsg.translate("QAD", "\nA symbol layer is a vector punctual layer without label.\n")
          self.showErr(errMsg)
-         return True # fine comando
+         return True # end command
       
       if  len(QadDimStyles.getDimListByLayer(currLayer)) > 0:
          errMsg = QadMsg.translate("QAD", "\nThe current layer belongs to a dimension style.\n")
          self.showErr(errMsg)
-         return True # fine comando
+         return True # end command
 
       if self.step == 0:     
          self.waitForEntsel(msgMapTool, msg)
-         return False # continua
+         return False # continue
 
 
       # =========================================================================
-      # RISPOSTA ALLA SELEZIONE DI UN'ENTITA' (da step = 0)
+      # RESPONSE TO ENTITY SELECTION (from step = 0)
       elif self.step == QadDIVIDECommandClassStepEnum.ASK_FOR_ENT:
          if self.entSelClass.run(msgMapTool, msg) == True:
             if self.entSelClass.entity.isInitialized():
-               # se il layer di destinazione è di tipo simbolo
+               # if the destination layer is a symbol type
                if qad_layer.isSymbolLayer(currLayer) == True:
-                  # se il simbolo può essere ruotato
+                  # if the symbol can be rotated
                   if len(qad_layer.get_symbolRotationFieldName(currLayer)) >0:
                      self.waitForAlignmentObjs()
                   else:
                      self.waitForSegmentNumber()
                return False
             else:
-               if self.entSelClass.canceledByUsr == True: # fine comando
+               if self.entSelClass.canceledByUsr == True: # end command
                   return True
                self.showMsg(QadMsg.translate("QAD", "No geometries in this position."))
                self.waitForEntsel(msgMapTool, msg)
-         return False # continua
+         return False # continue
       
 
       # =========================================================================
-      # RISPOSTA ALLA RICHIESTA DI ALLINEARE GLI OGGETTI (da step = ASK_FOR_ENT)
-      elif self.step == QadDIVIDECommandClassStepEnum.ASK_FOR_ALIGNMENT: # dopo aver atteso una parola chiave si riavvia il comando
-         if msgMapTool == True: # il punto arriva da una selezione grafica
-            if self.getPointMapTool().rightButton == True: # se usato il tasto destro del mouse
+      # RESPONSE TO ALIGN OBJECTS REQUEST (from step = ASK_FOR_ENT)
+      elif self.step == QadDIVIDECommandClassStepEnum.ASK_FOR_ALIGNMENT: # after waiting for a keyword the command restarts
+         if msgMapTool == True: # the point comes from a graphic selection
+            if self.getPointMapTool().rightButton == True: # if right mouse button is used
                value = self.defaultValue 
             else:
-               self.setMapTool(self.getPointMapTool()) # riattivo il maptool
+               self.setMapTool(self.getPointMapTool()) # reactivate the maptool
                return False
          else:
-            # la parola chiave arriva come parametro della funzione
+            # the keyword comes as a parameter of the function
             value = msg
 
          if type(value) == unicode:
@@ -273,18 +273,18 @@ class QadDIVIDECommandClass(QadCommandClass):
 
 
       # =========================================================================
-      # RISPOSTA ALLA RICHIESTA DEL NUMERO DI SEGMENTI (da step = ASK_FOR_ALIGNMENT)
+      # RESPONSE TO SEGMENT NUMBER REQUEST (from step = ASK_FOR_ALIGNMENT)
       # =========================================================================
-      elif self.step == QadDIVIDECommandClassStepEnum.ASK_SEGMENT_NUMBER: # dopo aver atteso un numero intero si riavvia il comando
-         if msgMapTool == True: # il punto arriva da una selezione grafica
-            if self.getPointMapTool().rightButton == True: # se usato il tasto destro del mouse
+      elif self.step == QadDIVIDECommandClassStepEnum.ASK_SEGMENT_NUMBER: # after waiting for an integer the command restarts
+         if msgMapTool == True: # the point comes from a graphic selection
+            if self.getPointMapTool().rightButton == True: # if right mouse button is used
                return False
             else:
-               self.setMapTool(self.getPointMapTool()) # riattivo il maptool
+               self.setMapTool(self.getPointMapTool()) # reactivate the maptool
                return False
          else:
-            # il numero di segmenti arriva come parametro della funzione
+            # the number of segments comes as a parameter of the function
             self.nSegments = msg
             self.doDivide(currLayer)
-            return True # fine comando
+            return True # end command
          return False
